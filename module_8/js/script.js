8a4a9542bc237dbbb66aaff9ed385b75fc047b6c
ctrangel <122327961+ctrangel@users.noreply.github.com>
2023-03-11 18:19:12 -0500

    assignment

---
+++ module_8/js/script.js
@@ -0,0 +1,36 @@
+let TVShow = {
+  name: "The Last of Us",
+  story_line:
+    "20 years after modern civilization has been destroyed, Joel, a hardened survivor, is hired to smuggle Ellie, a 14-year-old girl, out of an oppressive quarantine zone. What starts as a small job soon becomes a brutal heartbreaking journey as they both must traverse the U.S. and depend on each other for survival",
+  genre: "Drama",
+  actor: "Pedro Pascal",
+  actress: "Bella Ramsey",
+  release_date: "January 15, 2023",
+  creator: "Neil Druckmann",
+  where_to_watch: "HBO",
+  country_of_origin: "United States",
+  production_location: "Alberta, Canada"
+};
+
+document.write("Show: " + TVShow.name);
+document.write("<br>");
+document.write("<img src='media/tlou.jpg' width='200' height='200' >");
+document.write("<br>");
+document.write("Story Line: " + TVShow.story_line);
+document.write("<br>");
+document.write("Genre: " + TVShow.genre);
+document.write("<br>");
+document.write("Actor: " + TVShow.actor);
+document.write("<br>");
+document.write("Actress: " + TVShow.actress);
+document.write("<br>");
+document.write("Release Date: " + TVShow.release_date);
+document.write("<br>");
+document.write("Creator: " + TVShow.creator);
+document.write("<br>");
+document.write("Where to Watch: " + TVShow.where_to_watch);
+document.write("<br>");
+document.write("Country of Origin: " + TVShow.country_of_origin);
+document.write("<br>");
+document.write("Production Location: " + TVShow.production_location);
+
